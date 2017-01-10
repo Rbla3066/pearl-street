31311b3e2e3aadcaf340958d7aa6d3d9580bb168
Robert Black <rbla4655@gmail.com>
2017-01-10 16:24:47 -0500

    tap link

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -5,11 +5,6 @@ $(document).ready(function(){
 		e.preventDefault();
 		return false;
 	});
-	$("a").on("tap", function(e){
-		$(this).trigger("click");
-		e.preventDefault();
-		return false;
-	});
 });
 
 
